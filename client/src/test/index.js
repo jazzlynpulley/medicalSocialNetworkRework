40ab8be0bd62277f5c72ae62e837ac28104d844b
Cory-Pete <peter.cory0@gmail.com>
2019-02-27 15:57:40 -0700

    Updated the test case, it now runs regular login, invalid username, not found username, and incorrect password cases. All of the following are true about a container OS Template except...  You “boot it” like just like installation media on a physical machine

--- client/src/test/index.js
+++ client/src/test/index.js
@@ -12,6 +12,7 @@ async function run() {
         }
         
 	//const USERNAME_SELECTOR = '#root > div > div.container > div > div > form > div:nth-child(1) > label'
+	var names = ["notreal", "notreal@email.com", "fakepassword"];
 	const USERNAME_SELECTOR = '#email'
 	const PASSWORD_SELECTOR = '#password'
 	const BUTTON_SELECTOR1 = '#root > div > div.container.valign-wrapper > div > div > a.btn.btn-large.waves-effect.white.hoverable.black-text'
@@ -33,7 +34,24 @@ async function run() {
 
 	await page.waitForNavigation();
 	await page.click(BUTTON_SELECTOR3);
-
+	
+	var i;
+	for (i = 0; i < names.length; i++) { 
+		if(i == 2){
+			await page.type(USERNAME_SELECTOR, 'bob@email.com');
+			await page.type(PASSWORD_SELECTOR, 'fakefake');
+			await page.click(BUTTON_SELECTOR2);
+		}
+		else{
+  		await page.type(USERNAME_SELECTOR, names[i]);
+        	await page.type(PASSWORD_SELECTOR, names[i]);
+		await page.click(BUTTON_SELECTOR2);
+		await page.click(USERNAME_SELECTOR, {clickCount: 3})
+    		await page.keyboard.press('Backspace')
+		await page.click(PASSWORD_SELECTOR, {clickCount: 3})
+    		await page.keyboard.press('Backspace')
+	}}	
+	
         console.log('Test Executed Successfully');
         browser.close();
     }
